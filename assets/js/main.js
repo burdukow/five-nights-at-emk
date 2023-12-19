b97a0fceaba960ce469c4597372e9763d2b75325
burdukov <brdkv19@gmail.com>
2023-12-19 17:45:55 +0500

    added cam and door buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,12 @@
 let button_start = document.getElementById("start");
 let blocks = document.getElementsByClassName("block");
+
 button_start.addEventListener("click", function () {
     blocks[0].style.zIndex = 1;
     blocks[1].style.zIndex = 2;
+
+    DrawBackground("start");
+
+    CamButton(CamButtonRect);
+    DoorButton(DoorButtonRect);
 });
---
+++ assets/js/mousefunctions.js
@@ -0,0 +1,11 @@
+function getMousePos(canvas, event) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: event.clientX - rect.left,
+        y: event.clientY - rect.top,
+    };
+}
+
+function isInside(pos, rect) {
+    return pos.x > rect.x && pos.x < rect.x + rect.width && pos.y < rect.y + rect.height && pos.y > rect.y;
+}
