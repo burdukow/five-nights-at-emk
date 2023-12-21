f558d264646dacbbdf6f7ef0bc5580b77e4db19d
burdukov <brdkv19@gmail.com>
2023-12-21 14:36:12 +0500

    moved addCRTLines, added camera opening function, disabling start button

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ let blocks = document.getElementsByClassName('block');
 
 button_start.addEventListener('click', function () {
     blocks[0].style.zIndex = 1;
+    button_start.disabled = true;
     blocks[1].style.zIndex = 2;
     drawBackground('start');
     document.getElementById('gameZone').classList.add('on');
