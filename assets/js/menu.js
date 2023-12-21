46f53bf70c29ddddd31f270265a8027b77ea78ae
burdukov <brdkv19@gmail.com>
2023-12-21 14:36:32 +0500

    refactroing + vaping mechanics

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,22 +1,42 @@
 var image = new Image();
 
-var CamButtonRect = {
+var camButtonRect = {
     x: 600,
     y: 700,
     width: 400,
     height: 50,
 };
-var DoorButtonRect = {
+var doorButtonRect = {
     x: 100,
     y: 250,
     width: 100,
     height: 250,
 };
 
+var vapeProgressRect = {
+    x: 10,
+    y: 284,
+    width: 20,
+    height: 200,
+};
+
 function drawButton(rect) {
     context.beginPath();
     context.rect(rect.x, rect.y, rect.width, rect.height);
     context.fillStyle = 'rgba(225,225,225,0.5)';
+    context.fill();
+    context.lineWidth = 2;
+    context.strokeStyle = '#000000';
+    context.stroke();
+    context.closePath();
+}
+
+var color = 'rgba(0,255,0,1)';
+
+function drawProgressBar(rect, color) {
+    context.beginPath();
+    context.rect(rect.x, rect.y, rect.width, rect.height);
+    context.fillStyle = color;
     context.fill();
     context.lineWidth = 2;
     context.strokeStyle = '#000000';
@@ -35,9 +55,28 @@ function drawBackground(room) {
         image.src = './assets/img/rooms/placeholder.png'; // TODO: Add photos
         image.onload = function () {
             context.drawImage(image, 0, 0);
-            drawButton(CamButtonRect);
-            drawButton(DoorButtonRect);
             addCrtLines();
+            drawButton(camButtonRect);
+            drawButton(doorButtonRect);
+            drawProgressBar(vapeProgressRect, color);
+            let vapeInterval = setInterval(function () {
+                if (vapeProgress > 0) {
+                    vapeProgress -= 1;
+                    vapeProgressRect.height = vapeProgress;
+                    vapeProgressRect.y += 1;
+                    drawProgressBar(vapeProgressRect, color);
+                } else {
+                    alert('Death');
+                    clearInterval(vapeInterval);
+                }
+            }, 500);
         };
+    }
+}
+
+function addCrtLines() {
+    for (var y = 0; y < canvas.height; y += 3) {
+        context.fillStyle = 'rgba(0, 0, 0, .2)';
+        context.fillRect(0, y, canvas.width, 1);
     }
 }
