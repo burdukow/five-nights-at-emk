cd88832baf602a15f7b04467df69304d1ef3bafa
burdukov <brdkv19@gmail.com>
2023-12-21 13:14:25 +0500

    added more time to read, remove canvas class on

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,9 @@ button_start.addEventListener('click', function () {
     blocks[0].style.zIndex = 1;
     blocks[1].style.zIndex = 2;
     drawBackground('start');
-    document.getElementById('game').classList.add('on');
+    document.getElementById('gameZone').classList.add('on');
     setTimeout(function () {
         drawBackground('main');
-    }, 3000);
+        document.getElementById('gameZone').classList.remove('on');
+    }, 5000);
 });
